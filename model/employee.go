29b20ad6b4759550e85dea9b0b5441b7ada5eea1
Songvut Nakrong <songvut.nakrong@gmail.com>
2022-05-16 22:28:23 +0700

    Update *string in id

--- model/employee.go
+++ model/employee.go
@@ -1,11 +1,11 @@
 package model
 
 type Employee struct {
-	ID        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Salary    string `json:"salary"`
-	Age       string `json:"age"`
+	ID        *string `json:"id"`
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
+	Salary    string  `json:"salary"`
+	Age       string  `json:"age"`
 }
 
 type Employees struct {
